25b03f4bc9713d5de83af85d2195589566e4e098
GuiNegr <Guineg.git@outlook.com>
2024-04-17 13:48:21 -0300

    upando o carstore

---
+++ ESTUDOS/3 - SEMESTRE - PII JAVAWEB/carstore/src/main/java/br/com/carstore/dao/CarDao.java
@@ -0,0 +1,108 @@
+package br.com.carstore.dao;
+
+import br.com.carstore.model.Car;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class CarDao {
+
+    public void createCar(Car car){
+        String SQL = "INSERT INTO CAR (NAME) VALUES (?)";
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test","sa","sa");
+
+            System.out.println("sucesso na conexão");
+
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, car.getName());
+
+            preparedStatement.execute();
+            System.out.println("sucesso no insert");
+
+            connection.close();
+
+        }catch(Exception e){
+            System.out.println("error");
+        }
+
+
+
+    }
+
+    public List<Car> findAllCar(){
+
+        String SQL = "SELECT * FROM CAR";
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Car> cars = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                String carId = resultSet.getString("id");
+                String carName = resultSet.getString("name");
+
+                Car car = new Car(carId, carName);
+
+                cars.add(car);
+
+            }
+
+            System.out.println("success in select * car");
+
+            connection.close();
+
+            return cars;
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+            return Collections.emptyList();
+
+        }
+
+    }
+
+    public void deleteCarById(String carId) {
+        String SQL = "DELETE CAR WHERE ID = ?";
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, carId);
+            preparedStatement.execute();
+
+            System.out.println("success on delete car with id: " + carId);
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+        }
+    }
+
+
+}
